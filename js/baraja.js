640838d3fd577fd66f5919056245836c73296a42
Roberto <roberto.v.arebalo@gmail.com>
2023-11-13 20:57:52 -0300

    Agrego lo necesaro para comenzar a trabajar

---
+++ js/baraja.js
@@ -0,0 +1,11 @@
+/*La baraja española, que está compuesta por 48 cartas divididas en
+cuatro palos: oros, copas, espadas y bastos. Cada palo tiene 
+12 cartas, que van desde el as hasta el 10, y luego tres cartas 
+de figura: la sota, el caballo y el rey. Algunas variantes de 
+esta baraja incluyen dos comodines o cuatro cartas adicionales 
+llamadas 8 y 91*/
+
+const oros =  new Array(10).fill(0);
+const copas =  new Array(10).fill(0);
+const espadas =  new Array(10).fill(0);
+const bastos =  new Array(10).fill(0);
