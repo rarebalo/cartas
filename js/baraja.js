663efc3f6002f3b07261fb883a994d5c15fba6f7
Roberto <roberto.v.arebalo@gmail.com>
2023-11-14 07:47:24 -0300

    Se corrije el problema al mesclar el maso

--- js/baraja.js
+++ js/baraja.js
@@ -9,47 +9,45 @@ function naipes(cuarentaCartas) {
   const cartas = [];
   const figuras = ["sota", "caballo", "rey"];
   const palo = ["oro", "copa", "espada", "basto"];
-  const cartasPorPalo = cuarentaCartas? 10 : 12 ; 
-  
-    for (let paloIndex = 0; paloIndex < palo.length; paloIndex++) {
-        let i = 0;
-      for (let index = 0; index < cartasPorPalo; index++) {
-        
-        if (index < cartasPorPalo-3) {
-          cartas.push(index + 1 + " " + palo[paloIndex]);
-        } else {
-          cartas.push(palo[paloIndex] + " " + figuras[i]);
-          i++;
-        }
-        
+  const cartasPorPalo = cuarentaCartas ? 10 : 12;
+
+  for (let paloIndex = 0; paloIndex < palo.length; paloIndex++) {
+    let i = 0;
+    for (let index = 0; index < cartasPorPalo; index++) {
+      if (index < cartasPorPalo - 3) {
+        cartas.push(index + 1 + " " + palo[paloIndex]);
+      } else {
+        cartas.push(palo[paloIndex] + " " + figuras[i]);
+        i++;
       }
     }
-  
+  }
+
   return cartas;
 }
 
-function numeroAleatorio(min,max){
-    return Math.floor(Math.random() * (max - min +1)) + min;
+function numeroAleatorio(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function mesclarCartas(maso){
-    const masoOriginal = maso;
-    const masoMesclado = maso;
-    let carta = null;
+function mesclarCartas(maso) {
+  const masoMesclado = [...maso];
+  let carta = null;
 
-    for (let index = 0; index < masoOriginal.length; index++) {
-           carta = masoMesclado.splice(numeroAleatorio(0,masoOriginal.length),1)[0];  
-           
-           masoMesclado.splice(numeroAleatorio(0,masoMesclado.length),0, carta);   
-    }
-    return masoMesclado;
+  for (let index = 0; index < maso.length; index++) {
+    carta = masoMesclado.splice(numeroAleatorio(0, masoMesclado.length), 1)[0];
+    masoMesclado.splice(numeroAleatorio(0, masoMesclado.length), 0, carta);
+  }
+  return masoMesclado;
 }
 
-
-alert("Vamos a ver las cartas...")
+alert("Vamos a ver las cartas...");
 
 const masoUno = naipes(true);
 const masoUnoMesclado = mesclarCartas(masoUno);
 
-
-//Verificar por que cuando mescla las en algunas ocaciones aparecen cartas de la nada...
+for (let index = 0; index < masoUno.length; index++) {
+  
+  document.write(`${index} -> ${masoUno[index]} vs ${masoUnoMesclado[index]}<br>`)
+  
+}
